b50e6820d0a1e87a9eef8c648d8dddcd04596124
Christina Bannon <christinacbannon@gmail.com>
2021-06-13 21:16:13 -0400

    back to client

---
+++ client/src/app/config/config.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ConfigService } from './config.service';
+import  NeoVis  from 'neovis.js'; 
+
+@Component({
+  selector: 'app-config',
+  templateUrl: './config.component.html',
+  styleUrls: ['./config.component.css']
+})
+export class ConfigComponent implements OnInit {
+
+
+  constructor( private configService: ConfigService, private neovis: NeoVis) { }
+
+  ngOnInit(): void {
+    console.log('initted config')
+  }
+
+  myFunc(): void{
+    console.log('calling config service')
+    this.configService.getAll()
+      .subscribe(returnedThing => console.log(returnedThing));
+    console.log('done calling confid service')
+  }
+
+  draw() {
+    var viz; 
+      var config = {
+          container_id: "viz",
+          server_url: "bolt://localhost:7687",
+          server_user: "neo4j",
+          server_password: "sorts-swims-burglaries",
+          labels: {
+              "Character": {
+                  "caption": "name",
+                  "size": "pagerank",
+                  "community": "community"
+              }
+          },
+          relationships: {
+              "INTERACTS": {
+                  "thickness": "weight",
+                  "caption": false
+              }
+          },
+          initial_cypher: "MATCH (n)-[r:INTERACTS]->(m) RETURN *"
+      };
+
+      viz = new NeoVis(config);
+      viz.render();
+  }
+}
---
+++ client/src/app/neovis/neovis.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NeovisComponent } from './neovis.component';
+
+describe('NeovisComponent', () => {
+  let component: NeovisComponent;
+  let fixture: ComponentFixture<NeovisComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NeovisComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NeovisComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
